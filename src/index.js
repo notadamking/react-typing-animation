b5c492f8b4fbc8e3f121207255ceb4909cb1ffd2
Adam <adamjking3@gmail.com>
2017-05-10 00:57:54 -0600

    build subtree commit

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Typing from './Typing';
-
-export default Typing;
-export Backspace from './Backspace';
-export Cursor from './Cursor';
-export Delay from './Delay';
-export Reset from './Reset';
-export Speed from './Speed';
