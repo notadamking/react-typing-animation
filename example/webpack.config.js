b9004cc07be2e071fc83c38a1702cd793527c569
alonlevim <alonlevim@gmail.com>
2022-12-24 14:18:34 +0200

    Upgrade webpack 4

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
@@ -9,17 +8,15 @@ const isProduction = process.env.NODE_ENV !== 'development';
 module.exports = {
   devtool: isProduction ? 'source-map' : 'eval',
   context: path.resolve(__dirname, './'),
-  entry: isProduction
-    ? './index.js'
-    : ['webpack-hot-middleware/client', 'react-hot-loader/patch', './index.js'],
+  entry: isProduction ?
+    './index.js' : ['webpack-hot-middleware/client', 'react-hot-loader/patch', './index.js'],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, './build'),
     publicPath: isProduction ? '/react-typing-animation/' : '/',
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.html$/,
         loader: 'html-loader',
         options: {
@@ -32,27 +29,18 @@ module.exports = {
         loader: 'babel-loader',
       },
       {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: isProduction
-          ? ExtractTextPlugin.extract({
-              fallback: 'style-loader',
-              use: [
-                {
-                  loader:
-                    'css-loader?modules&importLoaders=1&localIdentName=[hash:base64:4]',
-                },
-                { loader: 'postcss-loader' },
-              ],
-            })
-          : [
-              { loader: 'style-loader' },
-              {
-                loader:
-                  'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-              },
-              { loader: 'postcss-loader' },
-            ],
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
       },
       {
         test: /\.(png|jpg|ico|woff|woff2|ttf|eot|svg)$/,
@@ -61,32 +49,28 @@ module.exports = {
     ],
   },
   plugins: [
-    new CopyWebpackPlugin([{ from: './public', to: './' }]),
+    new CopyWebpackPlugin([{
+      from: './public',
+      to: './'
+    }]),
     new HtmlWebpackPlugin({
       template: './template.html',
     }),
-    ...(isProduction
-      ? [
-          new webpack.DefinePlugin({
-            'process.env': {
-              NODE_ENV: JSON.stringify('production'),
-            },
-          }),
-          new ExtractTextPlugin({
-            filename: 'styles/main.css',
-            allChunks: true,
-            publicPath: 'styles/',
-          }),
-          new webpack.optimize.UglifyJsPlugin({
-            output: {
-              comments: false,
-            },
-          }),
-        ]
-      : [
-          new webpack.IgnorePlugin(/webpack-assets.json/),
-          new webpack.HotModuleReplacementPlugin(),
-        ]),
+    ...(isProduction ? [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production'),
+        },
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        output: {
+          comments: false,
+        },
+      }),
+    ] : [
+      new webpack.IgnorePlugin(/webpack-assets.json/),
+      new webpack.HotModuleReplacementPlugin(),
+    ]),
   ],
   stats: {
     children: false,
